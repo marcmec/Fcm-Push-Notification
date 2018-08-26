6301f7d199f3cc108988f4e8c26ca1ef810e1dca
marcmec <viniciuslag@gmail.com>
2018-08-26 13:12:29 -0300

    Message to multiple Devices Implementation

---
+++ SampleFCM/app/src/main/java/br/com/marcmec/samplefcm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,29 @@
+package br.com.marcmec.samplefcm;
+
+import android.app.Service;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Marcos Vinícius on 26/08/2018.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "FCM- TOKEN";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+
+    }
+
+
+}
